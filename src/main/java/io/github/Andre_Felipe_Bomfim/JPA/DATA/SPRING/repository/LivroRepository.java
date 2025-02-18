acee2b82ab5e3b3279d4b819f6e068e4ee1c9af2
andre <strongerandrebomfim@gmail.com>
2025-02-18 20:55:43 -0300

    feat: "implementa o JpaSpecificationExecutor para implementações com o Specification ou Specs"

--- src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/repository/LivroRepository.java
+++ src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/repository/LivroRepository.java
@@ -2,13 +2,15 @@ package io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.repository;
 
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.model.Autor;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.model.Livro;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-public interface LivroRepository extends JpaRepository<Livro, UUID> {
+public interface LivroRepository extends JpaRepository<Livro, UUID>, JpaSpecificationExecutor<Livro> {
 
 
     boolean existsByAutor(Autor autor);
