9d30d8fa20c92741dbcb1e90c30157bd5bc055d7
andre <strongerandrebomfim@gmail.com>
2025-02-18 20:53:49 -0300

    feat: "adciona GetMapping pesquisar no LivroController"

--- src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/controller/LivroCotroller.java
+++ src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/controller/LivroCotroller.java
@@ -3,13 +3,18 @@ package io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.controller;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.controller.mappers.LivroMapper;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.dto.CadastroLivroDTO;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.dto.ResultadoLivroDTO;
+import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.model.GeneroLivro;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.model.Livro;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.service.LivroService;
 import jakarta.validation.Valid;
+import org.aspectj.weaver.ast.Var;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("livros")
@@ -41,5 +46,33 @@ public class LivroCotroller implements GenericController {
                     var dto = mapper.toDto(livro);
                     return ResponseEntity.ok(dto);
                 }).orElseGet( () -> ResponseEntity.notFound().build() );
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deletar(@PathVariable
+                                        String id){
+        return service.obterPorId(UUID.fromString(id))
+                .map(livro -> {
+                    service.deletar(livro);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResultadoLivroDTO>> pesquisar(
+            @RequestParam(value = "isbn", required = false)
+            String isbn,
+            @RequestParam(value = "titulo", required = false)
+            String titulo,
+            @RequestParam(value = "nome-autor", required = false)
+            String nomeAutor,
+            @RequestParam(value = "genero", required = false)
+            GeneroLivro genero,
+            @RequestParam(value = "ano-publicacao", required = false)
+            Integer anoPublicacao)
+    {
+        var resultado = service.pesquisa(isbn, titulo, nomeAutor, genero, anoPublicacao);
+        var lista = resultado.stream().map(mapper::toDto).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
     }
 }
