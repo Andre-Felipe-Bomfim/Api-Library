5603eb8d48ee1c43eee1983512c36a1f1d809860
andre <strongerandrebomfim@gmail.com>
2025-04-27 20:54:14 -0300

    feat: "implementa o Optional findByIsbn para a busca por ISBN na aplicação"

--- src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/repository/LivroRepository.java
+++ src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/repository/LivroRepository.java
@@ -3,20 +3,21 @@ package io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.repository;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.model.Autor;
 import io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.model.Livro;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID>, JpaSpecificationExecutor<Livro> {
 
-
     boolean existsByAutor(Autor autor);
 
     List<Livro> findByAutor(Autor autor);
-    List<Livro> findByIsbn(String isbn);
+    Optional<Livro> findByIsbn(String isbn);
     List<Livro> findByTitulo(String titulo);
     List<Livro> findByTituloAndIsbn(String titulo, String isbn);
     List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
