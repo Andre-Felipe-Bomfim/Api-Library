84767af4208b5adf07adffb9c021984233def3f4
andre <strongerandrebomfim@gmail.com>
2025-05-25 21:41:35 -0300

    feat: "inclui a o Password encoder 'BCryptPasswordEncoder', além dos usuários salvbos na memória"

--- src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/config/SecurityConfiguration.java
+++ src/main/java/io/github/Andre_Felipe_Bomfim/JPA/DATA/SPRING/config/SecurityConfiguration.java
@@ -1,11 +1,18 @@
 package io.github.Andre_Felipe_Bomfim.JPA.DATA.SPRING.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,11 +23,37 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable) //importante para segurança
+                .httpBasic(Customizer.withDefaults()) // não é obrigatório
                 .formLogin(configurer -> {
                     configurer.loginPage("/login").permitAll();
                 }) //adiciona o formulario padrão, não é obrigatório
-                //.httpBasic(Customizer.withDefaults()) // não é obrigatório
-                .authorizeHttpRequests(authorize -> {authorize.anyRequest().authenticated();})
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();})
                 .build();
+    }
+
+    //para comparar senha digitada de senha em memória ou em um banco de dados
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);                                                                                                          //uma das mais seguras, quando você codifica a senha não é possível voltar atrás, ou seja, não tem descriptrografar
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        //em memória
+
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
